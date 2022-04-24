7a49820eecf518979812feb91d15a3cb246a5297
Shayan Ghazali <shayan97.tti@gmail.com>
2022-04-24 18:39:43 +0430

    category modification

--- src/app/upload/upload-category-modification/upload-category-modification.component.ts
+++ src/app/upload/upload-category-modification/upload-category-modification.component.ts
@@ -9,102 +9,52 @@ import { CardFormControls } from 'src/app/shared/model/model';
   styleUrls: ['./upload-category-modification.component.scss']
 })
 export class UploadCategoryModificationComponent {
-  formTitle = ['افزودن', 'شخص'];
-  requestRoute = 'lm/v1/person';
-  idAttributeKey = 'customerUUID';
+  formTitle = ['افزودن', 'دسته بندی رسانه'];
+  requestRoute = 'lm/v1/media/category';
+  idAttributeKey = 'categoryUUID';
   cardFormControls: CardFormControls[] = [
     {
-      'formControlName': 'firstName',
-      'fieldErrorMessage': 'بین 2 تا 30 حرف مجاز است',
+      'formControlName': 'name',
+      'fieldErrorMessage': 'بین 2 تا 70 حرف مجاز است',
       'persianLable': 'نام',
       'validation': {
         'isRequired': true,
         'minLength': 2,
-        'maxLength': 30,
+        'maxLength': 70,
       }
     },
     {
-      'formControlName': 'lastName',
-      'fieldErrorMessage': 'بین 2 تا 30 حرف مجاز است',
-      'persianLable': 'نام خانوادگی',
+      'formControlName': 'description',
+      'fieldErrorMessage': 'بیش از 150 حرف مجاز نیست',
+      'persianLable': 'توضیح',
       'validation': {
-        'isRequired': true,
-        'minLength': 2,
-        'maxLength': 30,
+        'isRequired': false,
+        'maxLength': 150,
       }
     },
     {
-      'formControlName': 'fatherName',
-      'fieldErrorMessage': 'بین 2 تا 30 حرف مجاز است',
-      'persianLable': 'نام پدر',
+      'field': {
+        type: 'select', objectAttribute:'category', requestRoute: 'lm/v1/media/category', objectLabel: [
+          {
+            persianKey: 'نام:',
+            attribute: 'name',
+            separator: ' - ',
+          },
+          {
+            persianKey: 'توضیح:',
+            attribute: 'description',
+            separator: '',
+          }
+        ]
+      },
+      'formControlName': 'categoryUUID',
+      'fieldErrorMessage': '',
+      'persianLable': 'دسته بندی رسانه',
       'validation': {
         'isRequired': true,
-        'minLength': 2,
-        'maxLength': 30,
       }
     },
-    {
-      'formControlName': 'nationalID',
-      'fieldErrorMessage': '10 رقم مجاز است',
-      'persianLable': 'کد ملی',
-      'validation': {
-        'isRequired': true,
-        'minLength': 10,
-        'maxLength': 10,
-        'pattern': '^[0-9]*$'
-      }
-    },
-    {
-      'formControlName': 'certificateNumber',
-      'fieldErrorMessage': 'بین 2 تا 30 رقم مجاز است',
-      'persianLable': 'شماره شناسنامه',
-      'validation': {
-        'isRequired': true,
-        'minLength': 2,
-        'maxLength': 12,
-        'pattern': '^[0-9]*$'
-      }
-    },
-    {
-      'formControlName': 'fromLocation',
-      'fieldErrorMessage': 'بین 2 تا 30 حرف مجاز است',
-      'persianLable': 'صادره',
-      'validation': {
-        'isRequired': true,
-        'minLength': 2,
-        'maxLength': 30,
-      }
-    },
-    {
-      'formControlName': 'stringBirthDate',
-      'fieldErrorMessage': 'xxxx/xx/xx فرمت مجاز می باشد',
-      'persianLable': 'تاریخ تولد',
-      'validation': {
-        'isRequired': true,
-        'pattern': '^[1-4]\\d{3}\\/((0[1-6]\\/((3[0-1])|([1-2][0-9])|(0[1-9])))|((1[0-2]|(0[7-9]))\\/(30|([1-2][0-9])|(0[1-9]))))$'
-      }
-    },
-    {
-      'formControlName': 'address',
-      'fieldErrorMessage': 'بین 2 تا 350 حرف مجاز است',
-      'persianLable': 'آدرس',
-      'validation': {
-        'isRequired': true,
-        'minLength': 2,
-        'maxLength': 350,
-      }
-    },
-    {
-      'formControlName': 'phoneNumber',
-      'fieldErrorMessage': '11 رقم مجاز است',
-      'persianLable': 'تلفن',
-      'validation': {
-        'isRequired': true,
-        'minLength': 11,
-        'maxLength': 11,
-        'pattern': '^[0-9]*$'
-      }
-    },
+   
   ];
 
   constructor() { }
