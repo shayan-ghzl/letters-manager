1d02440d1f81b8ef87015f9562135f475b6b9d77
shayan ghazali <shayan97.tti@gmail.com>
2024-06-30 13:51:42 +0330

    github dependabot alerts fixed

--- src/app/upload/upload.service.ts
+++ src/app/upload/upload.service.ts
@@ -2,7 +2,7 @@ import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
-import { AddMediaCategory, ImageParams, MediaCategory, Image } from '../shared/model/model';
+import { AddMediaCategory, Image, ImageParams, MediaCategory } from '../shared/model/model';
 
 @Injectable({
   providedIn: 'root'
